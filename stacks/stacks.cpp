0c2a1865ffc32677d9c2501998735e9ed176fc75
riteshpallod <ritesh.pallod7@gmail.com>
2017-12-28 18:27:33 +0530

    gitignore added;using vectors now; double repeating featureadded

--- stacks/stacks.cpp
+++ stacks/stacks.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <stdlib.h>
+#include <vector>
 
 using namespace std;
 
 struct Stack
 {
-    int *array;
+    vector<int> array;
     int top;
     int size;
 };
@@ -14,7 +15,7 @@ struct Stack* createStack(int size)
 {
     struct Stack *stack = (struct Stack *) malloc(sizeof(struct Stack));  //can use new
     stack->size = size;
-    stack->array = (int *) malloc(stack->size*sizeof(int));
+    stack->array.reserve(stack->size);
     stack->top = -1;
     return stack;
 } 
@@ -26,15 +27,27 @@ int isEmpty(struct Stack *stack)
     return 0; //false
 }
 
+void repeatedDoubling(struct Stack* stack)
+{
+    cout << "[doubling stack]" << endl;
+    stack->size = stack->size * 2;
+    stack->array.resize(stack->size);
+}
+
 void push(struct Stack *stack, int element)
 {
+    /*
     if (stack->top == (stack->size -1)) //full stack
         cout << "full stack" << endl;
     else 
     {
         stack->top++;
         stack->array[stack->top] = element; 
-    }    
+    }*/
+
+    repeatedDoubling(stack);
+    stack->top++;
+    stack->array[stack->top] = element;    
 }
 
 int pop(struct Stack* stack)
@@ -53,9 +66,9 @@ int pop(struct Stack* stack)
 int main()
 {
     struct Stack *stack;
-    stack = createStack(12);
+    stack = createStack(1);
 
-    cout << isEmpty(stack);
+    cout << isEmpty(stack) << endl;
     
     push(stack, 11);
     push(stack, 6);
